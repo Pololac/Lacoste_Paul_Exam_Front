e0e731f8677889d787c9a9d845551f6c36b62653
Paul Lacoste <lacostepaul@yahoo.fr>
2025-04-03 16:50:23 +0200

    "up"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { TarifComponent } from './pages/tarif/tarif.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'tarifs', component: TarifComponent}
+];
