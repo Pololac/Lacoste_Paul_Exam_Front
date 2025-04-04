6240a7c9bf1b3a2669321f95d5fe1970a6042b3f
Paul Lacoste <lacostepaul@yahoo.fr>
2025-04-04 14:07:00 +0200

    Router : ajout routes sans page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { TarifComponent } from './pages/tarif/tarif.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'tarifs', component: TarifComponent}
+    { path: 'tarifs', component: TarifComponent},
+    { path: 'services', component: NotFoundComponent},
+    { path: 'community', component: NotFoundComponent}
 ];
